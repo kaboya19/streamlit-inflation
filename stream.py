c38b0616d654eedfacff08b5c4f8bbbe2a7ecebc
Bora Kaya <kayabo19@itu.edu.tr>
2024-01-24 20:37:38 +0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -144,15 +144,10 @@ if page=='Yıllık Enflasyon':
     """,
     unsafe_allow_html=True
 )
-    col1, col2 = st.columns(2)
-
-# Sol sütunda siyah yazı
-    col1.markdown("Ocak Ayı Tahmini:", unsafe_allow_html=True)
-
-# Sağ sütunda kırmızı yazı
-    col2.markdown('<p style="color: red; margin: 0;">%6.60</p>', unsafe_allow_html=True)
-    st.write("**Güncelleme Tarihi:22 Ocak 2024**")
-    st.write("**Sonraki Güncelleme Tarihi:25 Ocak 2024**")
+    st.markdown('<p class="inline-text black-text">Ocak Ayı Enflasyon Tahmini:</p>', unsafe_allow_html=True)
+    st.markdown('<p class="inline-text red-text">%6.60(Önceki %6.47)</p>', unsafe_allow_html=True)
+    st.markdown('<p class="inline-text black-text">Güncelleme Tarihi:22 Ocak 2024</p>', unsafe_allow_html=True)
+    st.markdown('<p class="inline-text black-text">Sonraki Güncelleme Tarihi:25 Ocak 2024</p>', unsafe_allow_html=True)
     st.markdown("<h1 style='text-align:left;'>Yıllık Enflasyon Tahmini</h1>",unsafe_allow_html=True)
     st.plotly_chart(fig1)
 if page=='Aylık Enflasyon':
