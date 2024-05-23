ac693e2af0ebf7a646cacde30933ca73427d73cc
Bora Kaya <kayabo19@itu.edu.tr>
2024-05-23 22:16:31 +0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -46,8 +46,7 @@ fig1.update_traces(line=dict(width=3))
 fig1.update_layout(
     xaxis=dict(tickfont=dict(size=14, family="Arial Black", color="black")),  
     yaxis=dict(tickfont=dict(size=14, family="Arial Black", color="black")),
-    font=dict(family="Arial", size=14, color="black"),
-    margin=dict(l=0, r=0, t=150, b=150)  # Sol, sağ, üst ve alt kenar boşluklarını belirleme
+    font=dict(family="Arial", size=14, color="black")
 )
 
 fig1.update_xaxes(
@@ -64,8 +63,7 @@ fig1.update_xaxes(
 fig1.update_layout(
     autosize=False,
     width=1500,
-    height=1500,
-    margin=dict(l=50, r=50, t=50, b=50)
+    height=1500
 )
 
 
