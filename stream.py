0eb6bf9134d7d975e5ec511640a035d3b7831fbc
Bora Kaya <kayabo19@itu.edu.tr>
2024-05-23 22:07:58 +0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -163,7 +163,7 @@ if page=='Yıllık Enflasyon':
     st.markdown('<p class="inline-text black-text">Son Güncellenme Tarihi:23 Mayıs 2024</p>', unsafe_allow_html=True)
     st.markdown('<p class="inline-text black-text">Sonraki Güncellenme Tarihi:30 Mayıs 2024</p>', unsafe_allow_html=True)
     st.markdown("<h1 style='text-align:left;'>Yıllık Enflasyon Tahmini</h1>",unsafe_allow_html=True)
-    st.plotly_chart(fig1)
+    st.plotly_chart(fig1,width=2000,height=2000)
 if page=='Aylık Enflasyon':
     st.markdown("<h1 style='text-align:left;'>Aylık Enflasyon Tahmini</h1>",unsafe_allow_html=True)
     st.plotly_chart(fig2)
