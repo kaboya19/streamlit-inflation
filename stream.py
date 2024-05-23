85ccff7855d5db863d7536c8d91aa84d2f9e8071
Bora Kaya <kayabo19@itu.edu.tr>
2024-05-23 21:02:21 +0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -441,7 +441,7 @@ if page=="Performans Tablomuz":
    plt.annotate("6.83", xy=(4,gecmis.iloc[4,1]*0.97), fontweight="bold", fontsize=15)
    plt.annotate("4.53", xy=(4.7,gecmis.iloc[5,0]*0.95), fontweight="bold", fontsize=15)
    plt.annotate("3.70", xy=(5,gecmis.iloc[5,1]*0.95), fontweight="bold", fontsize=15)
-   plt.annotate("3.16", xy=(5.65,gecmis.iloc[6,1]*0.92), fontweight="bold", fontsize=15)
+   plt.annotate("3.16", xy=(5.65,gecmis.iloc[6,0]*0.95), fontweight="bold", fontsize=15)
    plt.annotate("3.76", xy=(6,gecmis.iloc[6,1]*0.95), fontweight="bold", fontsize=15)
    plt.annotate("3.18", xy=(6.6,gecmis.iloc[7,0]*0.95), fontweight="bold", fontsize=15)
    plt.annotate("3.11", xy=(7,gecmis.iloc[7,1]*0.95), fontweight="bold", fontsize=15)
