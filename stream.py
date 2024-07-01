348786ae18dee83b7b99f899d70ab39a3435aa5f
Bora Kaya <kayabo19@itu.edu.tr>
2024-07-02 01:08:19 +0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -37,9 +37,9 @@ del dfas["Unnamed: 0"]
 dfas=dfas.rename_axis(["Tarih"])
 
 fig1 = go.Figure()
-fig1.add_trace(go.Scatter(x=yıllıktahmin.index[13:24],y=yıllıktahmin["Ensemble"].iloc[13:24],mode='lines',name="Enflasyon"))
-fig1.add_trace(go.Scatter(x=yıllıktahmin.index[23:-1],y=yıllıktahmin["Ensemble"].iloc[23:-1],mode='lines',line_color='red',line=dict(dash='dash')))
-fig1.add_trace(go.Scatter(x=yıllıktahmin.index[24:-1],y=yıllıktahmin["Ensemble"].iloc[24:-1],mode='markers',name="Tahmin",marker=dict(size=10, color='orange')))
+fig1.add_trace(go.Scatter(x=yıllıktahmin.index[13:19],y=yıllıktahmin["Ensemble"].iloc[13:24],mode='lines',name="Enflasyon"))
+fig1.add_trace(go.Scatter(x=yıllıktahmin.index[18:-1],y=yıllıktahmin["Ensemble"].iloc[23:-1],mode='lines',line_color='red',line=dict(dash='dash')))
+fig1.add_trace(go.Scatter(x=yıllıktahmin.index[19:-1],y=yıllıktahmin["Ensemble"].iloc[24:-1],mode='markers',name="Tahmin",marker=dict(size=10, color='orange')))
 fig1.add_trace(go.Scatter(x=yıllıktahmin.index[15:25],y=[61.94,60.84,62.18,64.70,65.06,65.73,69.47,69.69,74.65],mode='markers',name="Geçmiş Tahminler",line_color="black"))
 fig1.update_traces(line=dict(width=3)) 
 
